48670a50310a0b21213c5a970aa2a2034cc4555f
ikii764 <rizkisaputra0764@gmail.com>
2026-03-10 21:04:55 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,27 @@
+import { useState } from "react"
+import Dashboard from "./pages/Dashboard"
+import Stock from "./pages/Stock"
+import History from "./pages/History"
+import Login from "./components/Login"
+import Sidebar from "./components/Sidebar"
 
+export default function App(){
+
+const [login,setLogin] = useState(false)
+const [page,setPage] = useState("dashboard")
+
+if(!login){
+return <Login setLogin={setLogin}/>
+}
+
+return(
+<div style={{display:"flex"}}>
+<Sidebar setPage={setPage}/>
+<div style={{flex:1,padding:30}}>
+{page==="dashboard" && <Dashboard/>}
+{page==="stock" && <Stock/>}
+{page==="history" && <History/>}
+</div>
+</div>
+)
+}
